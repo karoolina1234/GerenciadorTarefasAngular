c7cb25c9da98c327c8672391e1631d91114cf929
karoolina1234 <karoolina.sm@gmail.com>
2024-03-21 18:10:57 -0300

    add routes

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { TarefaService } from "./shared";
+import { ListarTarefaComponent } from './listar-tarefa/listar-tarefa.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarTarefaComponent
+  ],
   imports: [CommonModule],
   providers: [TarefaService],
 })
