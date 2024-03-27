6e7e2a4f31c4eeecfcf7bb490ba01533252b597f
karoolina1234 <karoolina.sm@gmail.com>
2024-03-27 15:19:02 -0300

    add function register task

--- src/app/tarefas/cadastrar-tarefa/cadastrar-tarefa.component.ts
+++ src/app/tarefas/cadastrar-tarefa/cadastrar-tarefa.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Tarefa, TarefaService } from "../shared";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-cadastrar-tarefa',
-  templateUrl: './cadastrar-tarefa.component.html',
-  styleUrls: ['./cadastrar-tarefa.component.css']
+  selector: "app-cadastrar-tarefa",
+  templateUrl: "./cadastrar-tarefa.component.html",
+  styleUrls: ["./cadastrar-tarefa.component.css"],
 })
 export class CadastrarTarefaComponent implements OnInit {
+  @ViewChild("formTarefa", { static: true }) formTarefa: NgForm; //o nome formTarefa foi declarado no html no form.
+  tarefa: Tarefa;
 
-  constructor() { }
+  constructor(private tarefaService: TarefaService, private router: Router) {}
 
   ngOnInit(): void {
+    this.tarefa = new Tarefa();
   }
 
+  cadastrar(): void {
+    if (this.formTarefa.form.valid) {
+      this.tarefaService.cadastrar(this.tarefa);
+      this.router.navigate(["/tarefas"]);
+    }
+  }
 }
